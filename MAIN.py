781078bcf7e1a3148e7c7ca6085b78f725f9ac9d
CJ Blastos <115042772+Sundownr40@users.noreply.github.com>
2022-11-15 12:50:05 -0800

    first commit

---
+++ MAIN.py
@@ -0,0 +1,16 @@
+###############################################################################################
+# SOURCES                                                                                     #
+# (https://coderslegacy.com/python/pygame)                                                    #
+# (https://api.arcade.academy/en/latest/examples/platform_tutorial/index.html)                #
+# (https://api.arcade.academy/en/latest/examples/platform_tutorial/step_08.html)              #
+# (https://pythonprogramming.net/pygame-start-menu-tutorial/)                                 #
+#                                                                                             #
+###############################################################################################
+
+#Global Variables
+import pygame
+from pygame.locals import *
+import sys
+import random
+import time
+
